ae5cf045dfdcbe998507e29663367e21279e770c
Ahsanul Kabir <kowshid@gmail.com>
2022-10-24 00:56:55 +0600

    Initial commit

---
+++ src/main/java/bkash/practice/PracticeApplication.java
@@ -0,0 +1,14 @@
+package bkash.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeApplication {
+
+	public static void main(String[] args) {
+		System.out.println("test project");
+		SpringApplication.run(PracticeApplication.class, args);
+	}
+
+}
